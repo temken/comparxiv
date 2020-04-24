50decc5e2182548193dc73aa0a7fe0e04c00ecba
Timon Emken <emken@chalmers.se>
2020-04-24 08:29:17 +0200

    Adds a new test.

--- comparxiv/tests/test_comparxiv.py
+++ comparxiv/tests/test_comparxiv.py
@@ -4,10 +4,6 @@ import os
 from ..comparxiv import *
 
 def test_comparxiv():
-	test_preprints = ["hep-ph/0612065","1709.06573"]
+	test_preprints = ["hep-ph/0612065","1709.06573","1905.05776"]
 	for ID in test_preprints:
-		if "/" in ID:
-			diff_file = os.path.split(ID)[-1]+"_v1v2"
-		else:
-			diff_file = ID+"_v1v2"
 		assert compare_preprints(ID, 1, 2, False, False, True)
